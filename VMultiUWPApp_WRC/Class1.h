6adab4637eda23af7a1af98580cf632a2f83ab69
Johnny Ernest <johnrussellernest@outlook.com>
2021-08-03 21:33:59 -0400

    Add files via upload

---
+++ VMultiUWPApp_WRC/Class1.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace VMultiUWPApp_WRC
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+    };
+}
---
+++ VMultiUWPApp_WRC/MyUserControl.xaml.h
@@ -0,0 +1,22 @@
+﻿//
+// MyUserControl.xaml.h
+// Declaration of the MyUserControl class
+//
+
+#pragma once
+
+#include "MyUserControl.g.h"
+
+namespace VMultiUWPApp_WRC
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class MyUserControl sealed
+	{
+	public:
+		MyUserControl();
+	private:
+		void slider_ValueChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs^ e);
+		void box_TextChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::TextChangedEventArgs^ e);
+		void button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
